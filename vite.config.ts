a808ba44fb1f48aa9d5963f6674704214e8b9ee2
nomeRebeca <rebecaoliver09@gmail.com>
2026-05-26 12:37:04 +0100

    ci: corrige erro de digitação na pasta do workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/task-app/', // 👈 IMPORTANTE: Põe o nome exato do teu repositório entre as barras!
+  base: '/tasks-app/', // 👈 IMPORTANTE: Põe o nome exato do teu repositório entre as barras!
 })
