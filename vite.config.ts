245b891fd4c94478fd7fed911d1bb3fa2befc60a
nomeRebeca <rebecaoliver09@gmail.com>
2026-05-26 13:38:11 +0100

    fix: remove base path para a vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tasks-app/', // 👈 IMPORTANTE: Põe o nome exato do teu repositório entre as barras!
+
 })
